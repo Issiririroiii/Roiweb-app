0f3e21e0e2b818715793a8e70d6fd23108094957
Issiririroiii <minusetri@gmail.com>
2025-04-23 14:53:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Roioi⚡⚡")
+st.title("Roi777⚡")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
