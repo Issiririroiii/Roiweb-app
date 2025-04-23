94bbd3721781f52c0203d13c0b4778f9f8677420
Issiririroiii <minusetri@gmail.com>
2025-04-23 14:59:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("Roi777⚡")
-st.write("Say no to drugs 🚫")
+st.write(        
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
