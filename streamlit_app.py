ece7b932ea29e017e97110fdd2ebc3c476587e38
Issiririroiii <minusetri@gmail.com>
2025-05-21 15:06:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 st.title("Roi777")
 st.write(        
-    "Wut Wut Wut"
+    "Bukan Web Judol"
 )
 #st.image("IMG-20250519-WA0016.jpg", width=2000)
 st.title("Roi pintar berhitung")
-st.header("sini cobain aja kalo ga percaya")
+st.header("nantang? sini cobain kalo ga percaya")
 angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
 
 if (angka % 2) == 0:
