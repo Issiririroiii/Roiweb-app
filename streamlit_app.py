504ee576d0905472117fb6667ab59a7552f6e1f7
Issiririroiii <minusetri@gmail.com>
2025-05-21 23:00:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,36 @@ if (angka % 2) == 0:
 else:
  st.write(f"{angka} Damn Hoki banget biasanya banyak yang suka, orangnya ganteng, cool, keren, kaya yg difoto 👆🏻")
 
+
+st.set_page_config(page_title="Suit Jawa", page_icon="✊")
+
+st.title("✊ Batu - ✋ Kertas- ✌️ Gunting")
+
+pilihan = ["Batu", "Kertas", "Gunting"]
+emoji = {
+    "Batu": "✊",
+    "Kertas": "✋",
+    "Gunting": "✌️"
+}
+
+st.markdown("Pilih salah satu dan lawan bot!")
+
+player_choice = st.selectbox("Pilihan kamu:", pilihan)
+if st.button("Main!"):
+    computer_choice = random.choice(pilihan)
+
+    st.write(f"Kamu memilih: {emoji[player_choice]} {player_choice}")
+    st.write(f"Komputer memilih: {emoji[computer_choice]} {computer_choice}")
+
+    if player_choice == computer_choice:
+        st.info("Hasil: Seri!")
+    elif (player_choice == "Batu" and computer_choice == "Gunting") or \
+         (player_choice == "Kertas" and computer_choice == "Batu") or \
+         (player_choice == "Gunting" and computer_choice == "Kertas"):
+        st.success("Hasil: Kamu menang, lawan bot aja bangga!")
+    else:
+        st.error("Hasil: si Bot menang nih masa kalah sama bot!")
+
+if st.button("Main Lagi yu"):
+    st.experimental_rerun()
+    
