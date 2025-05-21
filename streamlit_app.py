34fd49c74ff24c038dc295c306684d4cafa00855
Issiririroiii <minusetri@gmail.com>
2025-05-21 22:29:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,4 @@ if (angka % 2) == 0:
  st.write(f"{angka} Kurang Hoki dalam asmara, Bad boy/Bad Girl, redflag, bjir")
 else:
  st.write(f"{angka} Damn Hoki banget biasanya banyak yang suka, orangnya ganteng, cool, keren, kaya yg difoto 👆🏻")
+bgcolor("black")
